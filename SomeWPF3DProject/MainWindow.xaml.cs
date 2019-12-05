782b45beec9db2b8d33665653e9e2bc6b3a80529
MLX <maicoljjauregui@outlook.es>
2019-12-04 23:37:15 -0300

    Add files via upload

---
+++ SomeWPF3DProject/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Diagnostics;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Runtime.InteropServices;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FormsDraw = System.Drawing;
+
+namespace SomeWPF3DProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IntPtr hWnd;
+
+        [DllImport("user32.dll", SetLastError = false)]
+        static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(string lpClassName, string lpWindowName);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            MeshGeometry3D mesh = new MeshGeometry3D();
+            PointCollection textures = new PointCollection();
+            textures.Add(new Point(0, 0));
+            textures.Add(new Point(0, 1));
+            textures.Add(new Point(1, 1));
+            textures.Add(new Point(1, 0));
+            Point3DCollection asd = new Point3DCollection();
+            asd.Add(new Point3D(-1, 1, 0));
+            asd.Add(new Point3D(-1, -1, 0));
+            asd.Add(new Point3D(1, -1, 0));
+            asd.Add(new Point3D(1, 1, 0));
+            Int32Collection TIndex = new Int32Collection();
+            TIndex.Add(0);
+            TIndex.Add(1);
+            TIndex.Add(2);
+            TIndex.Add(0);
+            TIndex.Add(2);
+            TIndex.Add(3);
+            mesh.Positions = asd;
+            mesh.TriangleIndices = TIndex;
+            mesh.TextureCoordinates = textures;
+            v2dv3d.Geometry = mesh;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListWindows();
+        }
+
+        public void ChangeTransformAxis()
+        {
+            RotateTransform3D Rotation = new RotateTransform3D();
+            AxisAngleRotation3D RAxis = new AxisAngleRotation3D();
+            RAxis.Axis = new Vector3D(0, 1, 0);
+            RAxis.Angle = slider.Value;
+            Rotation.Rotation = RAxis;
+            v2dv3d.Transform = Rotation;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeTransformAxis();
+        }
+
+        public void ChangeWindow()
+        {
+            int handle;
+            Int32.TryParse(tb.Text, out handle);
+            if (String.IsNullOrWhiteSpace(tb.Text)){
+                hWnd = IntPtr.Zero;
+                //hWnd = GetDesktopWindow();//new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            }
+            else
+                hWnd = (IntPtr)handle;
+            WinCtl.Rect rc = new WinCtl.Rect();
+            WinCtl.GetWindowRect(hWnd, ref rc);
+            WinCtl img = new WinCtl(hWnd, rc);
+            v2dv3d.Visual = img;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+
+        public void ListWindows()
+        {
+            IntPtr thishWnd = new WindowInteropHelper(this).Handle;
+            winList.Items.Clear();
+            var hWnds = new List<IntPtr>();
+            WinUtils.EnumDelegate filter = delegate(IntPtr hWnd, int lParam)
+            {
+                string[] m_astrFilter = new string[] { "Start", "Program Manager" };
+                string title = WinUtils.GetWindowTitle(hWnd);
+                if (WinUtils.IsWindowVisible(hWnd) && !string.IsNullOrEmpty(title) && !m_astrFilter.Contains(title) && hWnd != thishWnd)
+                    hWnds.Add(hWnd);
+                return true;
+            };
+
+            if (WinUtils.EnumDesktopWindows(IntPtr.Zero, filter, IntPtr.Zero))
+            {
+                foreach (var hWnd in hWnds)
+                {
+                    ComboBoxItem win = new ComboBoxItem();
+                    win.Tag = hWnd;
+                    win.Content = WinUtils.GetWindowTitle(hWnd);
+                    winList.Items.Add(win);
+                }
+                winList.SelectedIndex = 0;
+            }
+        }
+
+        private void winList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (winList.SelectedItem as ComboBoxItem != null)
+            {
+                tb.Text = (winList.SelectedItem as ComboBoxItem).Tag.ToString();
+                ChangeWindow();
+            }
+
+        }
+    }
+
+    public class WinUtils
+    {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        public static ImageSource ImageSourceFromBitmap(FormsDraw.Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        public static string GetWindowTitle(IntPtr hWnd)
+        {
+            StringBuilder strbTitle = new StringBuilder(255);
+            int nLength = WinUtils.GetWindowText(hWnd, strbTitle, strbTitle.Capacity + 1);
+            string strTitle = strbTitle.ToString();
+            return strTitle;
+        }
+
+    }
+}
---
+++ SomeWPF3DProject/WinCtl.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+using System.Runtime.InteropServices;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FormsDraw = System.Drawing;
+using SWF = System.Windows.Forms;
+
+namespace SomeWPF3DProject
+{
+    public partial class WinCtl : Image
+    {
+        FormsDraw.Rectangle WinBounds;
+        string Title = "";
+        public WinCtl(IntPtr hwin, Rect rect)
+        {
+            InitializeComponent();
+            this.Tag = hwin;
+            WinBounds = new FormsDraw.Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            Title = GetWindowTitle(hwin);
+            this.Width = WinBounds.Width;
+            this.Height = WinBounds.Height;
+            if (hwin == IntPtr.Zero)
+                this.Source = CopyScreen();
+            else
+            {
+                this.Source = GetWindowImage(hwin, WinBounds);
+                DispatcherTimer dt = new DispatcherTimer(TimeSpan.FromMilliseconds(200), DispatcherPriority.Render, new EventHandler(TimerTick), this.Dispatcher);
+                //dt.Start();
+            }
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public void TimerTick(object sender, EventArgs e)
+        {            
+            Rect rect = new Rect();
+            GetWindowRect((IntPtr)this.Tag, ref rect);
+            WinBounds = new FormsDraw.Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            this.Width = WinBounds.Width;
+            this.Height = WinBounds.Height;
+            this.Source = GetWindowImage((IntPtr)this.Tag, WinBounds);
+        }
+
+        public string GetWindowTitle(IntPtr hWnd)
+        {
+            StringBuilder strbTitle = new StringBuilder(255);
+            int nLength = GetWindowText(hWnd, strbTitle, strbTitle.Capacity + 1);
+            return strbTitle.ToString();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public ImageSource GetWindowImage(IntPtr hWnd, FormsDraw.Rectangle bounds)
+        {
+            using (FormsDraw.Bitmap result = new FormsDraw.Bitmap(bounds.Width, bounds.Height))
+            {//creates a new Bitmap which will contain the window's image
+                using (FormsDraw.Graphics MemG = FormsDraw.Graphics.FromImage(result))
+                {//makes a Graphics from the Bitmap whose HDC will hold the window's RAW image
+                    IntPtr dc = MemG.GetHdc();//gets the Bitmap's HDC 
+                    try
+                    {
+                        PrintWindow(hWnd, dc, 0);//Writes the window's image to the HDC created above
+                    }
+                    finally
+                    {
+                        MemG.ReleaseHdc(dc);
+                    }
+                    var handle = result.GetHbitmap();
+                    try
+                    {
+                        return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    }
+                    finally { DeleteObject(handle); }
+                }
+            }
+            
+        }
+
+        public BitmapSource GetScreenImage()
+        {
+        using (FormsDraw.Bitmap screenBmp = new FormsDraw.Bitmap((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, FormsDraw.Imaging.PixelFormat.Format32bppRgb))
+            {
+                using (var bmpGraphics = FormsDraw.Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen(0, 0, 0, 0, screenBmp.Size);
+                    MessageBox.Show(screenBmp.Size.ToString());
+                    return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                        screenBmp.GetHbitmap(),
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+        }
+
+        public ImageSource CopyScreen()
+        {
+            var left = SWF.Screen.AllScreens.Min(screen => screen.Bounds.X);
+            var top = SWF.Screen.AllScreens.Min(screen => screen.Bounds.Y);
+            var right = SWF.Screen.AllScreens.Max(screen => screen.Bounds.X + screen.Bounds.Width);
+            var bottom = SWF.Screen.AllScreens.Max(screen => screen.Bounds.Y + screen.Bounds.Height);
+            var width = right - left;
+            var height = bottom - top;
+
+            using (var screenBmp = new FormsDraw.Bitmap(width, height, FormsDraw.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (var bmpGraphics = FormsDraw.Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen(left, top, 0, 0, new System.Drawing.Size(width, height));
+                    //return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(screenBmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    return ImageSourceFromBitmap(screenBmp);
+                }
+            }
+        }
+
+        public ImageSource ImageSourceFromBitmap(FormsDraw.Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                bmp.Save(@"C:\img.png");
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+    }
+}
